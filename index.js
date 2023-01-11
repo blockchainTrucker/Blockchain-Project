6ca925f81fe37fbcb7dcf0077309a40b87c1a453
Jesse Wachtel <jwachtel96@gmail.com>
2023-01-10 23:58:17 -0500

    1/10/23

--- index.js
+++ index.js
@@ -7,56 +7,92 @@ const crypto = require('crypto');
 const PORT = 5555;
 const nodeID = `http://localhost:${PORT}`;
 const { Blockchain, Transaction } = require('./blockchain');
-const validator = require('validator');
-const faucetPrivateKey =
-	'1d9f52460c220ef032f0e510082c21b01c8059a503080afc5ffd1aad48efc6d8';
-const faucetPublicKey =
-	'04439e9fc23cf27a2a03d44832e8d91a695224e6c780f959da09331368ed777e6dcfccb271de346239e83082064c44507fe5f40158dc077be8f5ed9573fe393713';
-const coin = new Blockchain();
+const axios = require('axios');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const node = (() => {
+	const faucetPrivateKey =
+		'1d9f52460c220ef032f0e510082c21b01c8059a503080afc5ffd1aad48efc6d8';
+	const faucetPublicKey =
+		'04439e9fc23cf27a2a03d44832e8d91a695224e6c780f959da09331368ed777e6dcfccb271de346239e83082064c44507fe5f40158dc077be8f5ed9573fe393713';
+	const coin = new Blockchain();
 
-app.get('/info', (req, res) => {
-	let info = coin.getInfo();
-	info.nodeID = nodeID;
-	res.send(JSON.stringify(info));
-});
+	let peers = [];
+	let miners = [];
 
-app.get('/debug', (req, res) => {
-	let info = coin.debug();
-	info.selfURL = `http://localhost:${PORT}`;
-	res.send(JSON.stringify(info));
-});
+	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(bodyParser.json());
 
-app.get('/reset-chain', (req, res) => {
-	coin.reset();
-	res.send(
-		JSON.stringify({
-			message: "The chain was reset to it's genesis block",
-		})
-	);
-});
+	app.get('/info', (req, res) => {
+		let info = coin.getInfo();
+		info.nodeID = nodeID;
+		res.send(JSON.stringify(info));
+	});
 
-app.post('/transaction', (req, res) => {
-	const transaction = req.body;
-	res.send(JSON.stringify(coin.addTransaction(transaction)));
-});
+	app.get('/debug', (req, res) => {
+		let info = coin.debug();
+		info.selfURL = `http://localhost:${PORT}`;
+		res.send(JSON.stringify(info));
+	});
 
-app.post('/wallet-balance', (req, res) => {
-	const address = req.body.address;
-	res.send(JSON.stringify(coin.getBalanceOfAddress(address)));
-});
+	app.get('/reset-chain', (req, res) => {
+		coin.reset();
+		res.send(
+			JSON.stringify({
+				message: "The chain was reset to it's genesis block",
+			})
+		);
+	});
 
-app.post('/faucet', (req, res) => {
-	const address = req.body.address;
-	coin.faucet(faucetPublicKey, address, faucetPrivateKey);
-	res.send(JSON.stringify(`sent 1 coin to ${address}`));
-});
+	app.post('/transaction', (req, res) => {
+		const transaction = req.body;
+		res.send(JSON.stringify(coin.addTransaction(transaction)));
+	});
 
-app.get('/pending-transactions', (req, res) => {
-	res.send(JSON.stringify(coin.pendingTransactions));
-});
+	app.post('/wallet-balance', (req, res) => {
+		const address = req.body.address;
+		res.send(JSON.stringify(coin.getBalanceOfAddress(address)));
+	});
 
-app.listen(PORT, () => {
-	console.log(`Server running at: http://localhost:${PORT}/`);
-});
+	app.post('/faucet', (req, res) => {
+		const address = req.body.address;
+		coin.faucet(faucetPublicKey, address, faucetPrivateKey);
+		res.send(JSON.stringify(`sent 1 coin to ${address}`));
+	});
+
+	app.get('/pending-transactions', (req, res) => {
+		res.send(JSON.stringify(coin.pendingTransactions));
+	});
+
+	app.post('/connect-miner', (req, res) => {
+		console.log(req.body);
+		res.send(
+			JSON.stringify({
+				previousBlockHash: coin.getLatestBlock().hash,
+				difficulty: coin.difficulty,
+				pendingTransactions: coin.pendingTransactions,
+			})
+		);
+	});
+
+	app.listen(PORT, () => {
+		console.log(`Node running at: ${nodeID}`);
+	});
+})();
+
+const miner = (async () => {
+	const PORT = 6000;
+	const minerID = `http://localhost:${PORT}`;
+	const publicKey =
+		'04016bb09f05461bb1da9f1891542e3f8eb05aa5babbd97ad8d97cb23b067c8db425e6d1740b87648ef117f2b8b7348f3a75737c1c8a9b396173fa0c40449de085';
+
+	app.use(bodyParser.urlencoded({ extended: false }));
+
+	app.listen(PORT, () => {
+		console.log(`Miner running at: ${minerID}`);
+	});
+
+	let chainData = await axios.post('http://localhost:5555/connect-miner', {
+		id: minerID,
+	});
+	console.log(chainData.data);
+})();
