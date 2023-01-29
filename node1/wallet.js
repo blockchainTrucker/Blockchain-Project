c97c1c49b1050633cb8d5a514348d51a95d1b25e
Jesse Wachtel <jwachtel96@gmail.com>
2023-01-29 13:43:56 -0500

    Update wallet.js

--- node1/wallet.js
+++ node1/wallet.js
@@ -5,6 +5,7 @@ const bs58 = require('bs58');
 const crypto = require('crypto');
 const hdkey = require('hdkey');
 const bip39 = require('bip39');
+const { create } = require('domain');
 
 // Generate a new master private key
 const mnemonic = bip39.generateMnemonic();
@@ -21,19 +22,56 @@ console.log('private key', privateKey.toString('hex'));
 // Get the public key
 const publicKey = ec.keyFromPrivate(privateKey).getPublic().encode('hex');
 console.log('public key', publicKey);
-
-const hashedPublicKey = new ripemd160()
-	.update(Buffer.from(publicKey, 'hex'))
-	.digest();
-console.log(hashedPublicKey.toString('hex'));
-const encodedHashedPublicKey = bs58.encode(new Uint8Array(hashedPublicKey));
-console.log(encodedHashedPublicKey.toString('hex'));
-
-// const decoded = bs58.decode(encodedHashedPublicKey);
+// const hashedPublicKey = new ripemd160()
+// 	.update(Buffer.from(publicKey, 'hex'))
+// 	.digest();
+// console.log(hashedPublicKey.toString('hex'));
+// const address = bs58.encode(new Uint8Array(hashedPublicKey));
+// console.log(address.toString('hex'));
 // const buffer = Buffer.from(decoded);
-
 // const hex = buffer.toString('hex');
 // console.log(hex);
+const getPublic = (address) => {
+	let decodedAddress = new Uint8Array(bs58.decode(address));
+
+	// Remove version byte and checksum
+	let publicKeyHash = decodedAddress.slice(1, -4);
+
+	// Reverse SHA-256
+	let revsha256 = crypto.createHash('sha256');
+	let revsha256Hash = revsha256.update(Buffer.from(publicKeyHash)).digest();
+
+	// Reverse RIPEMD-160
+	let revripemd160 = crypto.createHash('ripemd160');
+	let revripemd160Hash = revripemd160
+		.update(Buffer.from(revsha256Hash))
+		.digest();
+
+	console.log(revripemd160Hash.toString('hex'));
+};
+
+const createAddress = (publicKey) => {
+	let sha256 = crypto.createHash('sha256');
+	let sha256Hash = sha256.update(Buffer.from(publicKey, 'hex')).digest();
+
+	// Hash the result of SHA-256 using RIPEMD-160
+	let ripe = crypto.createHash('ripemd160');
+	let ripemd160Hash = ripe.update(Buffer.from(sha256Hash)).digest();
+
+	// Add version byte and checksum to create the address
+	let versionByte = new Uint8Array([0x00]);
+	let payload = Buffer.concat([versionByte, ripemd160Hash]);
+	let checksum = crypto
+		.createHash('sha256')
+		.update(crypto.createHash('sha256').update(payload).digest())
+		.digest()
+		.slice(0, 4);
+	let address = bs58.encode(Buffer.concat([payload, checksum]));
+	console.log(address);
+	getPublic(address);
+};
+createAddress(publicKey);
+
 // faucet
 // 886e673b75ef92cd252d7103a0a165940f1bc9f35923e1eb78ebaca54a7f1769
 // FPYjDRvWT21XbvYxtzNC3YMVgTY
